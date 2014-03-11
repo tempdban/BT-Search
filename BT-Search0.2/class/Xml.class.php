52b2ab208dd0dd19c757630e8e186e28457b68d0
kslr <kslrwang@gmail.com>
2014-03-12 01:17:06 +0800

    upload files

---
+++ BT-Search0.2/class/Xml.class.php
@@ -0,0 +1,19 @@
+<?php
+if(!defined('IN_SYS')) { exit('禁止访问'); exit(); }
+
+
+class XmlDom {
+
+	public $path
+
+	public function __construct($path='db', $file='tag.xml')
+	{
+		# code...
+	}
+
+	function open($file) {
+		__construct($file);
+	}
+}
+
+?>
